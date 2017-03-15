df7256d0b72089df9af492c3fbefa037256a0116
Pierce <pierce.dejong45@gmail.com>
2017-03-15 10:44:15 -0600

    Ayy what up boiiis

--- src/logic/Role.java
+++ src/logic/Role.java
@@ -10,10 +10,10 @@ public enum Role {
     MAFIA_HITMAN        ("Mafia: Hitman", "May kill someone each night", "To make the majority of the town mafia members"),
     DOCTOR              ("Doctor", "May heal one player each nigh", "To have only town members left"),
     SURVIVOR            ("Survivor", "Do nothing at night", "To be the last town member left alive"),
-    MAFIA_BARMAN        ("Mafia- Barman", "May stop the action of another player each night", "To make the majority of the town mafia members"),
+    MAFIA_BARMAN        ("Mafia: Barman", "May stop the action of another player each night", "To make the majority of the town mafia members"),
     BODYGUARD           ("Bodyguard", "May save another person by dying in their place", "To have only town members left"),
     LYNCHER             ("Lyncher", "Do nothing at night", "To Lynch a specific player to win solo win the game | "),
-    MAFIABOSS_GODFATHER ("Mafiaboss- GodFather", "Hidden from the Detective. Can send a message to another Mafia memeber each night", "To make the majority of the town mafia members"),
+    MAFIABOSS_GODFATHER ("Mafiaboss: GodFather", "Hidden from the Detective. Can send a message to another Mafia memeber each night", "To make the majority of the town mafia members"),
     VIGILANTE           ("Vigilante", "May kill new person each night", "To have only town members left");
 
     private final String roleName;

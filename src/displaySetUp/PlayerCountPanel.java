90d08166f10d01c2b9cae153e62a84a289a1c098
Pierce de Jong <pierce.dejong45@gmail.com>
2017-03-15 20:28:42 -0600

    doc

--- src/displaySetUp/PlayerCountPanel.java
+++ src/displaySetUp/PlayerCountPanel.java
@@ -13,13 +13,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import displayMain.MainController;
 import myJStuff.Colors;
 import myJStuff.MyButton;
 import myJStuff.MyButtonFont;
 import myJStuff.MyFont;
 import myJStuff.MyLabel;
 import net.miginfocom.swing.MigLayout;
-
+/**
+ * This class creates the Panel for selecting how many players you want in the game
+ * @author
+ *
+ */
 public class PlayerCountPanel{
 	
 	private Color textColor;
@@ -30,17 +35,24 @@ public class PlayerCountPanel{
 	private Font titleFont;
 	private Font btnFont;
 
+	//Panel that gets set to the frame and displays the contents of this class
 	private JPanel contentPane;
+	
+	//Panels that are added to the content pane. All JObjects get added to these panels
 	private JPanel north;
 	private JPanel south;
-	
-	private JLabel lblText;
-	private JLabel lblText2;
-	private JButton btnContinue;
 	private JPanel west;
 	private JPanel east;
 	private JPanel center;
 	
+	//Labels for the north panel
+	private JLabel lblText;
+	private JLabel lblText2;
+	
+	private JButton btnContinue;
+	private JButton btnBack;
+	
+	private int playerMinimum = 5;
 	private int playerTotal;
 	
 	private List<JButton> buttonList = new ArrayList<>();
@@ -49,14 +61,17 @@ public class PlayerCountPanel{
 	 * Create the frame.
 	 */
 	public PlayerCountPanel() {
+		//Sets the playerTotal to the minimum amount of players 
 		playerTotal = 5;
 		setFont();
 		setColor();
 
+		//Everything gets displayed on this panel 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		//These panels are what all JLabels, buttons etc. are added to
 		north = new JPanel();
 		contentPane.add(north, BorderLayout.NORTH);
 		north.setLayout(new MigLayout("", "[grow,center]", "[]"));
@@ -83,7 +98,9 @@ public class PlayerCountPanel{
 	}
 	
 	private void displayNorth(){
+		//Create the label and pass it text color and font
 		lblText = new MyLabel("How Many", textColor, titleFont);
+		//Add the label to the north panel at grid box 0,0 centering the text in the middle of the box
 		north.add(lblText, "cell 0 0,alignx center");
 		
 		lblText2 = new MyLabel("Playes?", textColor, titleFont);
@@ -91,30 +108,50 @@ public class PlayerCountPanel{
 	}
 
 	private void displayCenter(){
-		for(int i=5;i<11;i++){
+		//Loops to creat a button for each amount of players aloud 
+		for(int i=playerMinimum;i<11;i++){
+			//Create each button at location i
 			displayPlayerButton(i);
 		}
 	}
 	private void displaySouth(){
+		btnBack = new MyButton("Back", textColor, btnBackgroundColor, btnFont);
+		//south.add(btnBack, "cell 0 0");
+		btnBack.addActionListener(new ActionListener(){
+    		public void actionPerformed(ActionEvent e){
+    			MainController.getInstance().switchMain();
+		}});
+		//Create the button and pass it values for text, foreground and background color, and font
 		btnContinue = new MyButton("Continue", textColor, btnBackgroundColor, btnFont);
+		//Add the button to the south panel, button will fill width of screen 
 		south.add(btnContinue, "cell 0 0,growx");
+		//Add action listener for when the button is pressed
 		btnContinue.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
+    			//Switches the current panel to the game panel
     			SetUpController.getInstance().switchToGame(playerTotal);
 		}});
 		
 	}
-	
+	/**
+	 * Creates a button displaying option to pick amount of players
+	 * Button will be highlighted (blue) when pressed until another button is pressed
+	 * Sets the playerTotal to the value of i (amount of players)
+	 * @param i
+	 */
 	private void displayPlayerButton(int i){
 		JButton btnPlayer = new MyButton(Integer.toString(i), textColor, btnBackgroundColor, btnFont);
-		String position = "cell 0 "+i+",growx";
-		center.add(btnPlayer, position);
+		center.add(btnPlayer, "cell 0 "+i+",growx");
+		
 		btnPlayer.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
+    			//Sets background of all colors to default
     			for(int m=0;m<buttonList.size();m++){
     				buttonList.get(m).setBackground(btnBackgroundColor);
     			}
+    			//Sets color of the current button to blue when pressed 
     			btnPlayer.setBackground(selectColor);
+    			//Sets player total to value of button text
     			playerTotal = i;
 		}});
 		buttonList.add(btnPlayer);

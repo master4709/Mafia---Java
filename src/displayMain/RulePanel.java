757cd2228e04b40aaf065b1326f135b5ffba4e58
Mahsa Lotfi <mahsa.lotfi@gmail.com>
2017-03-17 17:32:31 +0000

    updated for buttonFont

--- src/displayMain/RulePanel.java
+++ src/displayMain/RulePanel.java
@@ -176,7 +176,7 @@ public class RulePanel{
 	 */
 	private void setFont(){
 		titleFont = new MyFont(50);
-		buttonFont = new MyButtonFont();
+		buttonFont = new MyFont(30);
 		textFont = new MyFont(30);
 	}
 	

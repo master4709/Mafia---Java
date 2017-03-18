879dcc385744798df8fe78e8644caa4094c02c2e
Mahsa Lotfi <mahsa.lotfi@gmail.com>
2017-03-18 03:13:33 +0000

    Documentation and minor changes

--- src/displayMain/MainPanel.java
+++ src/displayMain/MainPanel.java
@@ -20,40 +20,53 @@ import javax.swing.JButton;
 
 import net.miginfocom.swing.MigLayout;
 /**
- * 
- * @author
+ * This class creates panel for Main menu of Mafia game by creating different panels and putting them together. 
+ * Each panel contains buttons or labels. Each button is assigned to an action.
+ * @author Mahsa Lotfi
  *
  */
 public class MainPanel{
-	
+	/**
+	 * Instance variables
+	 */
+	//background color instance variable
 	private Color backgroundColor;
 	
+	//Panel that gets set to the frame and displays the contents of this class
 	private JPanel contentPane;
+	
+	//Panels that are added to the content pane. All JObjects get added to these panels
 	private JPanel north;
 	private JPanel west;
 	private JPanel east;
 	private JPanel south;
 	private JPanel center;
 	
-	private JButton btnNewGame;
-	private JButton btnDebug;
+	//buttons for center panel
+	private JButton btnNewGame;	
 	private JButton btnContinueGame;
 	private JButton btnAbout;
 	private JButton btnRules;
+	
+	//Jlabel that contains background image
 	private JLabel lblMan;
+	
+	//button for south panel
+	private JButton btnDebug;
 
 	/**
-	 * Create the application.
+	 * Create the main panel frame.
 	 */
 	public MainPanel() {
-
-		setFont();
+		//setting color
 		setColor();
 		
+		//creating content pane which holds all the panels together.
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(2, 2, 2, 2));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		//creating layout for each panels
 		north = new JPanel();
 		contentPane.add(north, BorderLayout.NORTH);
 		north.setLayout(new MigLayout("", "[grow]", "[]"));
@@ -71,23 +84,23 @@ public class MainPanel{
 		contentPane.add(center, BorderLayout.CENTER);
 		center.setLayout(new MigLayout("", "[grow]", "[][][][][]"));
 		
-		displayTop();
-		displayLeft();
-		displayRight();
-		displayBottom();
+		//displaying contents of each panels
+		displayNorth();
+		displaySouth();
 		displayCenter();
 		
+		//setting background of each panel.
 		setBackground(backgroundColor);
 	}
 
 	/**
-	 * Initialize the contents of the 
+	 * Method to display content of north panel, which is an image.
 	 */
 	
-	private void displayTop(){
+	private void displayNorth(){
 
 		//TODO img = ImageIO.read(new File("strawberry.jpg"));
-
+		//adding image to background
 		ImageIcon icon = new ImageIcon("data/pictures/mafia.png");
 		lblMan = new JLabel(icon);
 
@@ -95,17 +108,14 @@ public class MainPanel{
 		north.add(lblMan, "center");
 	}
 
-	private void displayLeft(){
-		
-	}
 	
-	private void displayRight(){
-		
-	}
-	
-	private void displayBottom(){
+	/**
+	 * Method to display contents of south panel. This panel contains the debug button.
+	 */
+	private void displaySouth(){
 		btnDebug = new MyButton("Debug is " + Debug.amOn());
 		south.add(btnDebug, "cell 0 0 ,growx");
+		// setting the action for the button
 		btnDebug.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				Debug.toggle();
@@ -114,9 +124,13 @@ public class MainPanel{
 		});
 	}
 	
+	/**
+	 * Method to display content of center panel. It will set up buttons. 
+	 */
 	private void displayCenter(){
 		btnNewGame = new MyButton("New Game");
 		center.add(btnNewGame, "cell 0 1,growx");
+		// setting the action for new game button
 		btnNewGame.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
     			MainController.getInstance().switchToSetUp();
@@ -124,6 +138,7 @@ public class MainPanel{
 		
 		btnContinueGame = new MyButton("Continue Game");
 		center.add(btnContinueGame, "cell 0 2,growx");
+		// setting the action for continue game button
 		btnContinueGame.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
 				
@@ -131,6 +146,7 @@ public class MainPanel{
 		
 		btnRules = new MyButton("Rules");
 		center.add(btnRules, "cell 0 3,growx");
+		// setting the action for Rules button
 		btnRules.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
     			MainController.getInstance().switchRule();
@@ -138,6 +154,7 @@ public class MainPanel{
 		
 		btnAbout = new MyButton("About");
 		center.add(btnAbout, "cell 0 4,growx");
+		// setting the action for about button
 		btnAbout.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e){
     			MainController.getInstance().switchAbout();
@@ -160,13 +177,17 @@ public class MainPanel{
 		contentPane.setBackground(Colors.defaultBorderColor);
 	}
 	
-	private void setFont(){
-	}
 	
+	/**
+	 * Method to set the background color.
+	 */
 	private void setColor(){
 		backgroundColor = Colors.defaultBackgroundColor;
 	}
 	
+	/**
+	 * Getter method for the content pane.
+	 */
 	public JPanel getContentPane(){
 		return contentPane;
 	}

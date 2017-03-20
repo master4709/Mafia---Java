f4ce009ad5f5e98d89e84aed91e63f3975548bb2
xtilyn <MrRobot29065>
2017-03-20 18:04:56 +0000

    dox

--- src/logic/RoleAssignment.java
+++ src/logic/RoleAssignment.java
@@ -2,35 +2,30 @@ package logic;
 import java.util.*;
 
 /**
- * This class will create randomized list of roles and its assiciated info & goal
- * based on roles user selected.
  *
- * Players will each have a unique role.
+ * @author Christilyn Arjona, 30033435
  */
 public class RoleAssignment extends Debug {
 
-	protected int totalPlayers;
+	int totalPlayers;
 
 	private List<Role> chosenRoles = new ArrayList<>();
 
 	/**
 	 * Class Constructor Specifies the initial number of total players,
-	 * initializes the roles based on total number of players.
 	 *
 	 * @param numOfPlayers num total players
 	 */
 	RoleAssignment(int numOfPlayers) {
 		totalPlayers = numOfPlayers;
-		System.out.println("num of total players: " + totalPlayers);
 	}
 
-	/**
-	 * Asks user which roles are to be included in the game and generates
-	 * a randomized order of these roles which is stored in the private field assignments.
-	 *
-	 * @see #randomizeRoles()
+	/***
+	 * Converts a specified list of type String to a list of their corresponding enum objects.
+	 * Store result in chosenRoles list
+	 * @param chosenRolesString A list of type String for the selected roles
 	 */
-	protected void playerAssignment(List<String> chosenRolesString){
+	void playerAssignment(List<String> chosenRolesString){
 
 		for (String role : chosenRolesString) {
 			this.chosenRoles.add(findRoleByID(role));
@@ -38,6 +33,11 @@ public class RoleAssignment extends Debug {
 
 	}
 
+	/***
+	 * Converts a specified String of roleID to its corresponding enum object
+	 * @param roleID roleID
+	 * @return Role object
+	 */
 	private Role findRoleByID(String roleID) {
 
 		for (Role role : Role.values()) {
@@ -49,7 +49,10 @@ public class RoleAssignment extends Debug {
 
 	}
 
-	protected List<Role> getChosenRoles() {
+	/***
+	 * @return A List of type Role
+	 */
+	List<Role> getChosenRoles() {
 		return chosenRoles;
 	}
 

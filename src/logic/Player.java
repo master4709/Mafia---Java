3a71499260fcfd75d7436bcaa570887fc4c18cd1
Mahsa Lotfi <mahsa.lotfi@gmail.com>
2017-03-20 16:58:37 +0000

    Doc

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -2,29 +2,33 @@ package logic;
 
 
 /**
- * This class stores the data of each player
- * Author: Mahsa Lotfi Gaskarimahalleh, UCID#10072013
+ * This class stores the data of each player. All these data is going to be used
+ * in a list that contain all the data for players. 
+ * Author: Mahsa Lotfi, UCID#10072013
  */
 public class Player {
 	
+	/**
+	 * Instance variables.
+	 */
 	private int playPosition;
-	private int playerTarget;
-	private int oldPlayerTarget;
-	private String name;
-	private String role;
-	private String roleInfo;
-	private String goal;
-	private boolean isMafia;
-	private boolean isDead;
-	private boolean isHealed;
-	private boolean isTargeted;
-	private boolean isProtected;
+	private int playerTarget;//target of player to be lynched
+	private String name;//name of the player
+	private String role;//role of the player
+	private String roleInfo;//info for each role which is instruction related to the role
+	private String goal;//the goal of each role
+	private boolean isMafia;//true if the person is mafia member
+	private boolean isDead;//true if the player is dead
+	private boolean isHealed;//true if the player is healed
+	private boolean isTargeted;//true if the player is targeted by mafia
+	private boolean isProtected;//true if the bodygaurd protect the player to be lynched
 	private boolean inBar;//If true the player cannot do anything that night.
-	private boolean isLynched;
+	private boolean isLynched;//true if the player is lynched.
 	
 	
-	
-	//Setters//
+	/**
+	 * Setter methods.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -34,9 +38,7 @@ public class Player {
 	public void setPlayerTarget(int playerTarget){
 		this.playerTarget = playerTarget;
 	}
-	public void setOldPlayerTarget(int oldPlayerTarget) {
-		this.oldPlayerTarget = oldPlayerTarget;
-	}
+	
 	public void setRole(String role){
 		this.role = role;
 	}
@@ -69,8 +71,9 @@ public class Player {
 	}
 	
 	
-	
-	//Getters//
+	/**
+	 * getter methods.
+	 */
 	public String getName() {
 		return name;
 	}
@@ -80,9 +83,7 @@ public class Player {
 	public int getPlayerTarget(){
 		return playerTarget;
 	}
-	public int getOldPlayerTarget(){
-		return oldPlayerTarget;
-	}
+	
 	public String getRole(){
 		return role;
 	}

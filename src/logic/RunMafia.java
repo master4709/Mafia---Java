8d908102dea3835fef5f59e47d1035c2c0b5f1cd
nestr <nestr@10.13.92.187>
2017-03-20 09:40:44 -0600

    tried to change the icon

--- src/logic/RunMafia.java
+++ src/logic/RunMafia.java
@@ -1,12 +1,15 @@
 package logic;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import displayMain.MainController;
 /**
  * This class runs the entire game
- * @authors Christilyn, Elvin, Masha, Pierce, Ronelle 
+ * @authors Team 18: Christilyn, Elvin, Masha, Pierce, Ronelle 
  */
-public class RunMafia {
+public class RunMafia extends Debug {
 	
 	public static JFrame frame = new JFrame();
 	
@@ -19,6 +22,11 @@ public class RunMafia {
 	private static void makeFrame(){
 		int width = 480;
 		int height = 852;
+		Image image = new ImageIcon("../../data/icon.png").getImage();
+		try {
+			frame.setIconImage(image);
+		} catch(Exception e){ $("Image not found.");}
+
 		frame.setBounds(550, 20, width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}

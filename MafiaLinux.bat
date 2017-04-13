123b897067278ed72e351a10e188dcb7db0c4500
Pierce de Jong <pierce.dejong45@gmail.com>
2017-04-13 12:12:56 -0600

    FINAL PUSH. this is the final version of Mafia

--- MafiaLinux.bat
+++ MafiaLinux.bat
@@ -1,4 +1,5 @@
 ECHO Mafia 4.20: A CPSC 233 Project.
-javac -d ./ -verbose -classpath ".:src\myJStuff\miglayout15-swing.jar" src\displayGame\*.java src\displayMain\*.java src\logic\*.java src\displaySetUp\*.java src\myJStuff\*.java src\playerInfo\*.java src\run\*.java
-java -cp "src\myJStuff\miglayout15-swing.jar:" run.RunMafia
+javac -d src -verbose -classpath ".:miglayout15-swing.jar" src\displayGame\*.java src\displayMain\*.java src\logic\*.java src\displaySetUp\*.java src\myJStuff\*.java src\playerInfo\*.java src\run\*.java
+cd src
+java -cp "miglayout15-swing.jar:" run.RunMafia
 pause
--- MafiaWindows.bat
+++ MafiaWindows.bat
@@ -1,5 +1,5 @@
 ECHO Mafia 4.20: A CPSC 233 Project.
-javac -d ./ -verbose -classpath ".;miglayout15-swing.jar" src\displayGame\*.java src\displayMain\*.java src\logic\*.java src\displaySetUp\*.java src\myJStuff\*.java src\playerInfo\*.java src\run\*.java
-cd bin
+javac -d src -verbose -classpath ".;miglayout15-swing.jar" src\displayGame\*.java src\displayMain\*.java src\logic\*.java src\displaySetUp\*.java src\myJStuff\*.java src\playerInfo\*.java src\run\*.java
+cd src
 java -cp "miglayout15-swing.jar;" run.RunMafia
 pause
